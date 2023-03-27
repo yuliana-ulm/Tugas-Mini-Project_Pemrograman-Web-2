6c6ced6f2e30346233f6dd36a7b809f28aef41ad
Yuliana <112523858+yuliana-ulm@users.noreply.github.com>
2023-03-28 00:32:38 +0700

    mencoba

--- app/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-define('BASEURL', 'http://localhost/phpmvc/public');
-
-// DB
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
-define('DB_NAME', 'phpmvc');
